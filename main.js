4170d8a1356985c94e574fc6f28c4a4b684b7be2
Saik2009 <80210806+Saik2009@users.noreply.github.com>
2022-05-14 09:18:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+
+}
+function setup(){
+    video= createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
+}
+function draw(){
+}
