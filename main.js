89b4ee304d7ac333cb75468e725a43f046b4322b
Saik2009 <80210806+Saik2009@users.noreply.github.com>
2022-05-15 11:44:17 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
-function preload(){
+song = "";
+song1="";
+right_wrist_y = 0;
+right_wrist_x = 0;
+left_wrist_x = 0;
+left_wrist_y = 0;
+
+function preload() {
+    song = loadSound("Tmmc.mp3");
+    song1 = loadSound("Ch.mp3");
+
 
 }
-function setup(){
-    video= createCapture(VIDEO);
-    video.size(550,500);
-    canvas=createCanvas(550,500);
-    canvas.position(560,150);
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.position(350,250);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelReady);
+    poseNet.on('pose', getPoses);
 }
-function draw(){
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play_song() {
+
+}
+
+function modelReady() {
+    console.log("poseNet has been initalized!")
+}
+
+function getPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        left_wrist_x = results[0].pose.leftWrist.x;
+        left_wrist_y = results[0].pose.leftWrist.y;
+        right_wrist_x = results[0].pose.rightWrist.x;
+        right_wrist_y = results[0].pose.rightWrist.y;
+        console.log(" Left_Wrist_X = " + left_wrist_x + " Left_Wrist_Y = " + left_wrist_y);
+        console.log(" Right_Wrist_X = " + right_wrist_x + " Right_Wrist_Y = " + right_wrist_y);
+    }
 }
